5edc67a71a9701feacc41436c6af7e491a993f55
Grant Orchard <go@hashicorp.com>
2020-07-27 20:03:10 +1000

    Added DNS module for the account move

--- main.tf
+++ main.tf
@@ -44,14 +44,32 @@ module "security_group_outbound" {
   tags = var.tags
 }
 
-module "dns" {
-  source = "github.com/lhaig/terraform-dns-multicloud"
-
-  owner ="Grant Orchard"
-  namespace = "go"
-  hosted-zone = "hashidemos.io"
-  create_aws_dns_zone = true
-  aws_region = "ap-southeast-2"
+data "aws_route53_zone" "main" {
+  name = "hashidemos.io"
 }
 
+# AWS SUBZONE
 
+resource "aws_route53_zone" "aws_sub_zone" {
+  name    = "go.hashidemos.io"
+  comment = "Managed by Terraform, Delegated Sub Zone for AWS for go.hashidemos.io"
+
+  tags = {
+    name       = "go.hashidemos.io"
+    owner      = "Grant Orchard"
+    created-by = "Grant Orchard"
+    ttl        = "-1"
+  }
+}
+
+resource "aws_route53_record" "aws_sub_zone_ns" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "go.hashidemos.io"
+  type    = "NS"
+  ttl     = "30"
+
+  records = [
+    for awsns in aws_route53_zone.aws_sub_zone.name_servers :
+    awsns
+  ]
+}

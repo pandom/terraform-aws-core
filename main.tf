a9c27d75a06c900b43747475a887eb04c06c2e14
Anthony Burke <pandom@users.noreply.github.com>
2020-10-07 10:14:07 +1100

    Waiting to peer with Grant

--- main.tf
+++ main.tf
@@ -105,15 +105,15 @@ resource "aws_route53_record" "aws_sub_zone_ns" {
   ]
 }
 # Grant's VPC
-resource "aws_vpc_peering_connection" "foo" {
-  peer_vpc_id   = local.vpc_id
-  vpc_id        = module.vpc.vpc_id
+// resource "aws_vpc_peering_connection" "foo" {
+//   peer_vpc_id   = local.vpc_id
+//   vpc_id        = module.vpc.vpc_id
 
-  accepter {
-    allow_remote_vpc_dns_resolution = true
-  }
+//   accepter {
+//     allow_remote_vpc_dns_resolution = true
+//   }
 
-  requester {
-    allow_remote_vpc_dns_resolution = true
-  }
-}
+//   requester {
+//     allow_remote_vpc_dns_resolution = true
+//   }
+// }

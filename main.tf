ab5911a3d0416e718dee7378ab9637c0908e5d48
Anthony Burke <pandom@users.noreply.github.com>
2020-10-07 14:49:36 +1100

    Changes

--- main.tf
+++ main.tf
@@ -73,37 +73,37 @@ resource "aws_key_pair" "key" {
   tags = var.tags
 }
 
-data "aws_route53_zone" "main" {
-  name = "burkey.hashidemos.io"
-}
+// data "aws_route53_zone" "main" {
+//   name = "hashidemos.io"
+// }
 
-# AWS SUBZONE
+// # AWS SUBZONE
 
-resource "aws_route53_zone" "aws_sub_zone" {
-  for_each = toset(var.sub_zone)
-  name    = each.value
-  comment = "Managed by Terraform, Delegated Sub Zone for AWS for burkey.hashidemos.io"
+// resource "aws_route53_zone" "aws_sub_zone" {
+//   for_each = toset(var.sub_zone)
+//   name    = each.value
+//   comment = "Managed by Terraform, Delegated Sub Zone for AWS for burkey.hashidemos.io"
 
-  tags = {
-    name       = "burkey"
-    owner      = "Anthony Burke"
-    created-by = "Anthony Burke"
-    ttl        = "-1"
-  }
-}
+//   tags = {
+//     name       = "burkey"
+//     owner      = "Anthony Burke"
+//     created-by = "Anthony Burke"
+//     ttl        = "-1"
+//   }
+// }
 
-resource "aws_route53_record" "aws_sub_zone_ns" {
-  for_each = toset(var.sub_zone)
-  zone_id = data.aws_route53_zone.main.zone_id
-  name    = each.value
-  type    = "NS"
-  ttl     = "30"
+// resource "aws_route53_record" "aws_sub_zone_ns" {
+//   for_each = toset(var.sub_zone)
+//   zone_id = data.aws_route53_zone.main.zone_id
+//   name    = each.value
+//   type    = "NS"
+//   ttl     = "30"
 
-  records = [
-    for awsns in aws_route53_zone.aws_sub_zone[each.value].name_servers :
-    awsns
-  ]
-}
+//   records = [
+//     for awsns in aws_route53_zone.aws_sub_zone[each.value].name_servers :
+//     awsns
+//   ]
+// }
 # Grant's VPC
 // resource "aws_vpc_peering_connection" "foo" {
 //   peer_vpc_id   = local.vpc_id
